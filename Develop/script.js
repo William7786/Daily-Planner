a8d9f54373098bb8e2e29d4953a42c90e7b3931b
Tcomp <williamt7786@gmail.com>
2021-05-01 10:07:25 -0400

    this commit was to fix github pages

--- Develop/script.js
+++ Develop/script.js
@@ -4,5 +4,10 @@ console.log(today.format("ddd MMM do  YYYY"))
 $("#time").text(today.format("h:mm a"))
 console.log(today.format("h:mm a"))
 
+var text = document.getElementById ("text")
+let chores = []
+
+localStorage.setItem('text', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('text'))
 
   
