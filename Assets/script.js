a1232e1de023b221c889f2187b2a1195b28b108c
Tcomp <williamt7786@gmail.com>
2021-05-03 03:14:46 -0400

    this commit was for a rework of my js to add jquery variables due to old functions not working for every section

--- Assets/script.js
+++ Assets/script.js
@@ -5,22 +5,41 @@ $("#time").text(today.format("h:mm a"))
 console.log(today.format("h:mm a"))
 
 
-const storage = document.querySelector(".store");
-const text = document.querySelector(".text2");
-const btn = document.querySelector(".saveBtn");
-const stored = localStorage.getItem("text", text.textContent)
-if(storage){
-text.textContent = stored
-}   
-storage.addEventListener("input", p => {
-text.textContent = p.target.value
-console.log(p.target.value)
-})
-const save = () => {
-localStorage.setItem("text", text.textContent)
-}
 
-btn.addEventListener("click", save)
+$("#9 .stored").val(localStorage.getItem("9"));
+$("#10 .stored").val(localStorage.getItem("10"));
+$("#11 .stored").val(localStorage.getItem("11"));
+$("#12 .stored").val(localStorage.getItem("12"));
+$("#1 .stored").val(localStorage.getItem("1"));
+$("#2 .stored").val(localStorage.getItem("2"));
+$("#3 .stored").val(localStorage.getItem("3"));
+$("#4 .stored").val(localStorage.getItem("4"));
+$("#5 .stored").val(localStorage.getItem("5"));
+
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".stored").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+})
+
+
+//this comment is my old code withour jquery//
+// const storage = document.querySelector(".store");
+// const text = document.querySelector(".text2");
+// const btn = document.querySelector(".saveBtn");
+// const stored = localStorage.getItem("text", text.textContent)
+// if(storage){
+// text.textContent = stored
+// }   
+// storage.addEventListener("input", p => {
+// text.textContent = p.target.value
+// console.log(p.target.value)
+// })
+// const save = () => {
+// localStorage.setItem("text", text.textContent)
+// }
+
+// btn.addEventListener("click", save)
 
 
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ First I applied the general styling and container elements to the page.
 Added moment time to the top of the page working on refreshing time.      
 Added more styling to the page.
 added local storage and added more styling to the page to fit lots of text. 
+Had to rework html and script functions to get all input areas to save.  
+added new id's 
+added new class to p element
